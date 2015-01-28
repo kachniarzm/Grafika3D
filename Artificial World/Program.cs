3c9f42747cfde907699c4388018d56983e0fdbe9
Maciej Kachniarz <kachniarzm@student.mini.pw.edu.pl>
2015-01-28 16:16:32 +0100

    bugfix

--- Artificial World/Program.cs
+++ Artificial World/Program.cs
@@ -96,12 +96,12 @@ namespace Artificial_World
 
         readonly float[] _cubeRockTexcoords =
         {
+            1, 1,  0, 1,   0, 0,  1, 0, // front (from light side)
             0, 0,  0, 1,   1, 1,  1, 0, // right (from start persp)
             0, 0,  1, 0,   1, 1,  0, 1, // top
             1, 0,  0, 0,   0, 1,  1, 1, // left
             0, 0,  1, 0,   1, 1,  0, 1, // bottom
-            0, 1,  1, 1,   1, 0,  0, 0  // back
-            1, 1,  0, 1,   0, 0,  1, 0, // front (from light side)
+            0, 1,  1, 1,   1, 0,  0, 0,  // back          
         };
 
         readonly float[] _cubeAdvTexcoords =
